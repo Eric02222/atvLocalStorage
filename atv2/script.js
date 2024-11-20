9cb390984ceea31c94cafbeb9f593fa975e07aa1
Eric Mara <114702151+Eric02222@users.noreply.github.com>
2024-11-19 22:05:39 -0300

    Add files via upload

--- atv2/script.js
+++ atv2/script.js
@@ -1,21 +1,26 @@
-// Adiciona um ouvinte de evento que será executado quando o documento for totalmente carregado
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('Documento carregado e pronto.');
     const form = document.getElementById('cadastroForm');
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         const nome = document.getElementById('nome').value;
         const email = document.getElementById('email').value;
         const senha = document.getElementById('senha').value;
-        const usuario = { nome, email, senha };
         let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        // Gerar um ID único para o novo usuário
+        // Se houver usuários cadastrados, define o ID como o próximo na sequência. Caso contrário, define o ID como 1.
+        let id = usuarios.length ? usuarios[usuarios.length - 1].id + 1 : 1;
+        // Verifica se o ID já está em uso. Se estiver, incrementa o ID até encontrar um valor único.
+        while (usuarios.some(usuario => usuario.id === id)) {
+            id++;
+        }
+        const usuario = { id, nome, email, senha };
         usuarios.push(usuario);
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
         modal.style.display = "block";
         form.reset();
     });
 });
-
 // Obtém o modal
 let modal = document.getElementById("myModal");
 
@@ -23,15 +28,12 @@ let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
 
 // Quando o usuário clicar no <span> (x), fecha o modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
-
 // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-
